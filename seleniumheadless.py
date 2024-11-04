f83c8ee376401c9ead1c2b7ac2e68532aa7c52ed
SensationalAbitbul <149378094+SensationalAbitbul@users.noreply.github.com>
2024-11-04 17:02:37 +0200

    עדכון 04.11.24 - 17:00

--- seleniumheadless.py
+++ seleniumheadless.py
@@ -5,27 +5,34 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+from selenium.webdriver.chrome.options import Options
 
+service = Service(executable_path=r'/usr/bin/chromedriver')
+options = Options() #webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument("--disable-dev-shm-using")
+options.add_argument("--remote-debugging-port=9222")
+options.add_argument('--disable-crash-reporter')
+options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://google.com")
-
+#driver = webdriver.Chrome('/snap/chromium/2985/usr/lib/chromium-browser/chromedriver')
 WebDriverWait(driver, 5).until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, "gLFyf"))
 )
 
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
 input_element.clear()
-input_element.send_keys("Good Luck Charm" + Keys.ENTER)
+input_element.send_keys("Rick Roll" + Keys.ENTER)
 
 WebDriverWait(driver, 5).until(
-    EC.presence_of_all_elements_located((By.PARTIAL_LINK_TEXT, "Good"))
+    EC.presence_of_all_elements_located((By.PARTIAL_LINK_TEXT, "Rick"))
 )
 
 
-link = driver.find_element(By.PARTIAL_LINK_TEXT, "Good")
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Rick")
 link.click() 
 
 time.sleep(10)
-
+print(driver.current_url)
 driver.quit()
